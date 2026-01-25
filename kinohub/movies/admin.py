0117fed68057e88391efd88f1a8953f31ca173ca
justdawy <davidshevnya@ukr.net>
2026-01-25 21:12:58 +0200

    feat: create movie player genre actor item models

--- kinohub/movies/admin.py
+++ kinohub/movies/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Movie, Player, Genre, Actor, Item
+
+admin.site.register(Movie)
+admin.site.register(Player)
+admin.site.register(Genre)
+admin.site.register(Actor)
+admin.site.register(Item)
--- kinohub/movies/models.py
+++ kinohub/movies/models.py
@@ -1,3 +1,61 @@
 from django.db import models
 
-# Create your models here.
+class Genre(models.Model):
+    name = models.CharField(
+        max_length=100,
+        unique=True,
+        help_text="Enter a movie genre (e.g. Drama, Horror, Comedy)"
+    )
+
+    def __str__(self):
+        return self.name
+
+class Actor(models.Model):
+    name = models.CharField(
+        max_length=100,
+        unique=True,
+        help_text="Enter an actor's name (e.g. Tobey Maguire)"
+    )
+
+    def __str__(self):
+        return self.name
+
+
+
+
+
+
+class Movie(models.Model):
+    title = models.CharField(max_length=255)
+    description = models.TextField(blank=True, null=True)
+    image_url = models.URLField(blank=True, null=True)
+    full_quality = models.CharField(max_length=50, blank=True, null=True)
+    imdb = models.FloatField(blank=True, null=True)
+    release_year = models.PositiveIntegerField(blank=True, null=True)
+    genres = models.ManyToManyField(
+            Genre, help_text="Select a genre for this book"
+    )
+    actors = models.ManyToManyField(
+            Actor, help_text="Select an actor for this movie"
+    )
+
+
+    def __str__(self):
+        return self.title
+
+class Player(models.Model):
+    movie = models.ForeignKey(Movie, related_name='players', on_delete=models.CASCADE)
+    title = models.CharField(max_length=80)
+
+    def __str__(self):
+        return self.title + "\n" + self.movie.title
+
+class Item(models.Model):
+    player = models.ForeignKey(Player, related_name='items', on_delete=models.CASCADE)
+    url = models.URLField()
+    episode_number = models.PositiveIntegerField(blank=True, null=True)
+
+    def __str__(self):
+        if self.episode_number:
+            return self.player.title + '\n' + self.player.movie.title + f"\nEpisode {self.episode_number}"
+        return f"{self.player.title}"
