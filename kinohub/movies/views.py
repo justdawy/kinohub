15c60966d1cc2655e9e26550277fdd2b41b30c45
justdawy <davidshevnya@ukr.net>
2026-02-02 22:29:25 +0200

    refactor: convert index view to class-based view

--- kinohub/movies/views.py
+++ kinohub/movies/views.py
@@ -1,15 +1,19 @@
 from django.shortcuts import render
 from django.db.models import Prefetch
+from django.views import generic
 
 from movies.models import Movie, Category
 
-def index(request):
-    movies = Prefetch(
-        "movies",
-        queryset=Movie.objects.all().order_by("-created_on")[:8],
-        to_attr="first_movies"
-    )
-    categories = Category.objects.prefetch_related(movies).filter(
-        is_visible_on_home=True).order_by("position")
-    context = {"categories": categories}
-    return render(request, "movies/index.html", context=context)
+class IndexView(generic.ListView):
+    template_name = "movies/index.html"
+    context_object_name = "categories"
+    
+    def get_queryset(self):
+        movies = Prefetch(
+            "movies",
+            queryset=Movie.objects.all().order_by("-created_on")[:8],
+            to_attr="first_movies"
+        )
+        categories = Category.objects.prefetch_related(movies).filter(
+            is_visible_on_home=True).order_by("position")
+        return categories
