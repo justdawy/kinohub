58b4d7ab70c1ee0ef1ec4410d586834c5d2cc4d6
justdawy <davidshevnya@ukr.net>
2026-02-02 22:18:42 +0200

    chore: configure Category admin display options

--- kinohub/movies/admin.py
+++ kinohub/movies/admin.py
@@ -2,7 +2,12 @@ from django.contrib import admin
 
 from .models import Movie, Player, Genre, Actor, Item, Category
 
-admin.site.register(Category)
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ("name", "is_visible_on_home", "position")
+    list_filter = ("is_visible_on_home",)
+    ordering = ("position",)
+    
 admin.site.register(Movie)
 admin.site.register(Player)
 admin.site.register(Genre)
