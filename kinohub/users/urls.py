7440c6857e9b60550cc25dc0f90a5270edf9ea0e
justdawy <davidshevnya@ukr.net>
2026-03-25 20:06:46 +0000

    refactor(users): rewrite registration modal using HTMX, closes #13

--- kinohub/users/urls.py
+++ kinohub/users/urls.py
@@ -2,14 +2,12 @@ from django.urls import include, path
 
 from users.views import (
     AjaxPasswordResetView,
-    AjaxSignupView,
     UserProfileTemplateView,
     create_review,
     profile_edit,
 )
 
 urlpatterns = [
-    path("signup/", AjaxSignupView.as_view(), name="signup"),
     path(
         "password/reset/",
         AjaxPasswordResetView.as_view(),
